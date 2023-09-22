822a037a4577d11f9eabbdf845ec24414f6e9438
Sakshi Singh <sakshisingh.ss58@gmail.com>
2023-09-22 10:23:39 +0530

    Add files via upload

---
+++ src/java/NewClass.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class NewClass 
+{
+    public static void main(String args[])
+    {
+        Random rm = new Random();
+        System.out.println(rm.nextInt());
+    }
+}
